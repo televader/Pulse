44ff13cf9aa3f54d06980948b626382c8c411508
televader <denispalacios@gmail.com>
2025-11-25 22:29:23 -0300

    Cambios en mensajes de errores

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -15,8 +15,29 @@ export async function apiClient(path, options = {}) {
   const res = await fetch(url, config);
 
   if (!res.ok) {
+    let message = `Error ${res.status}`;
+
+    switch (res.status) {
+      case 403:
+        message = "No tienes permisos para acceder a este recurso.";
+        break;
+      case 500:
+        message = "Ocurrió un error en el servidor. Intenta nuevamente.";
+        break;
+      default:
+        message = `Error inesperado (${res.status}).`;
+    }
+
+    const error = new Error(message);
+    error.status = res.status; // 🔥 CLAVE para manejo en React Query
+    error.url = url;
+
+    throw error;
+  }
+  
+ /*  if (!res.ok) {
     throw new Error(`API error ${res.status} at ${url}`);
   }
-
+ */
   return res.json();
 }
